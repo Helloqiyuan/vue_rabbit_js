09fe1e33fd0da01ccd3bb0917d4fbea479829fd8
Helloqiyuan <3302663850@qq.com>
2025-11-08 19:54:16 +0800

    home懒加载指令实现-copy

--- src/main.js
+++ src/main.js
@@ -1,16 +1,33 @@
 // import './assets/main.css'
 // 引入自定义css样式
-import '@/styles/common.scss'
+import "@/styles/common.scss";
+import { useIntersectionObserver } from '@vueuse/core'
 
-import { createApp } from 'vue'
-import { createPinia } from 'pinia'
+import { createApp } from "vue";
+import { createPinia } from "pinia";
 
-import App from './App.vue'
-import router from './router'
+import App from "./App.vue";
+import router from "./router";
 
-const app = createApp(App)
+const app = createApp(App);
 
-app.use(createPinia())
-app.use(router)
+app.use(createPinia());
+app.use(router);
 
-app.mount('#app')
+app.mount("#app");
+
+app.directive("img-lazy", {
+  mounted(el, binding) {
+    // el: 指令绑定的那个元素 img
+    // binding: binding.value  指令等于号后面绑定的表达式的值  图片url
+    console.log(el, binding.value);
+    const { stop } = useIntersectionObserver(el, ([{ isIntersecting }]) => {
+      console.log(isIntersecting);
+      if (isIntersecting) {
+        // 进入视口区域
+        el.src = binding.value;
+        stop();
+      }
+    });
+  },
+});
