0c7b8cd5fc56ee401d512b99216050135acd92a5
Helloqiyuan <3302663850@qq.com>
2025-11-13 19:38:37 +0800

    pinia用户数据持久化 pinia-plugin-persistedstate

--- src/stores/user.js
+++ src/stores/user.js
@@ -1,15 +1,21 @@
 import { ref } from "vue";
 import { defineStore } from "pinia";
 import { loginApi } from "@/apis/user";
-export const useUserStore = defineStore("user", () => {
-  const userInfo = ref({});
-  const getUserInfo = async ({ account, password }) => {
-    const res = await loginApi({ account, password });
-    userInfo.value = res.result;
-    console.log("(Pinia)登录返回结果:",res.result);
-  };
-  return {
-    userInfo,
-    getUserInfo,
-  };
-});
+export const useUserStore = defineStore(
+  "user",
+  () => {
+    const userInfo = ref({});
+    const getUserInfo = async ({ account, password }) => {
+      const res = await loginApi({ account, password });
+      userInfo.value = res.result;
+      console.log("(Pinia)登录返回结果:", res.result);
+    };
+    return {
+      userInfo,
+      getUserInfo,
+    };
+  },
+  {
+    persist: true,
+  }
+);
