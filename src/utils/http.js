38a1b41df13690d34a07b595382e22ad6993e02b
Helloqiyuan <3302663850@qq.com>
2025-11-14 14:38:19 +0800

    401拦截处理

--- src/utils/http.js
+++ src/utils/http.js
@@ -1,29 +1,40 @@
-import axios from 'axios'
-import { ElMessage } from 'element-plus'
-import 'element-plus/theme-chalk/el-message.css'
-import { useUserStore } from '@/stores/user'
+import axios from "axios";
+import { ElMessage } from "element-plus";
+import "element-plus/theme-chalk/el-message.css";
+import { useUserStore } from "@/stores/user";
+import router from "@/router";
 const httpInstance = axios.create({
-  baseURL: 'http://pcapi-xiaotuxian-front-devtest.itheima.net',
-  timeout: 5000
-})
+  baseURL: "http://pcapi-xiaotuxian-front-devtest.itheima.net",
+  timeout: 5000,
+});
 
 // axios请求拦截器
-httpInstance.interceptors.request.use(config => {
-  const userStore = useUserStore()
-  const token = userStore.userInfo.token
-  if(token){
-    config.headers.Authorization = `Bearer ${token}`
-  }
-  return config
-}, e => Promise.reject(e))
+httpInstance.interceptors.request.use(
+  (config) => {
+    const userStore = useUserStore();
+    const token = userStore.userInfo.token;
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (e) => Promise.reject(e)
+);
 
 // axios响应式拦截器
-httpInstance.interceptors.response.use(res => res.data, e => {
-  ElMessage({
-    type: 'error',
-    message: e.response.data.message,
-  })
-  return Promise.reject(e)
-})
+httpInstance.interceptors.response.use(
+  (res) => res.data,
+  (e) => {
+    const userStore = useUserStore();
+    ElMessage.warning(e.response.data.message);
+    // 401报错
+    console.log(e);
+    if (e.response?.status === 401) {
+      userStore.ClearUserInfo();
+      router.push("/login");
+    }
+    return Promise.reject(e);
+  }
+);
 
-export default httpInstance
+export default httpInstance;
